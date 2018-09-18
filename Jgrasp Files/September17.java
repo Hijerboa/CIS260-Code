84622c32167bf666a2e68679e2b415d1f585e9ac
Nick Cleary <nicholascleary01@gmail.com>
2018-09-18 10:49:08 -0400

    Adding first files

---
+++ Jgrasp Files/September17.java
@@ -0,0 +1,20 @@
+class September17 {
+  public static void main(String args[]) {
+    /*if (y > 1){
+      x = 2;
+    }else {
+      x = 4;
+    }
+  
+  
+    if (y>1){
+      x = 2;
+    }
+  
+    if (y <= 1) {
+      x = 4;
+    }*/
+    
+    
+  }
+}
---
+++ Jgrasp Files/September18.java
@@ -0,0 +1,17 @@
+class September18 {
+  public static void main(String args[]) {
+    int sum = 0;
+    int n = 0; 
+    int runs = 0;
+    /*while (sum < 1000){
+      runs++;
+      System.out.println(runs);
+      sum = sum + n;
+      n = n + 2;
+    }*/
+    while (sum < 1000);{
+      sum = sum +n;
+      n = n + 2;
+    }
+  }
+}
