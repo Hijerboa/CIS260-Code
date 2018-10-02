e418bac15ac1d9606eb666b19c6f8645552ee0a6
Nick Cleary <nicholascleary01@gmail.com>
2018-10-02 10:31:19 -0400

    things

---
+++ Jgrasp Files/October2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class October2{
+   public static void main(String args[]){
+      //System.out.println("hi");
+      Scanner input = new Scanner(System.in);
+      int x = input.nextInt();
+      int sum = 0;
+      /*while (x > 10) {
+         sum = sum + x--;
+      }*/
+      /*do{
+         sum = sum + x--;
+      }while (x>10);*/
+      /*if(x > 10) {
+         sum = sum + x;
+      } 
+      do{
+         sum + x--;
+      }while (x>10);*/
+      //System.out.println(sum);
+      
+   }
+}
---
+++ Jgrasp Files/lab5ec.java
@@ -0,0 +1,5 @@
+public class lab5ec{
+   public static void main(String[] args) {
+   
+   }
+}
