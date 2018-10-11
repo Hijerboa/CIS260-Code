1bb0016b394117886988cfd4f6a043d36671b6b3
Nick Cleary <nicholascleary01@gmail.com>
2018-10-11 10:18:22 -0400

    wow more things

---
+++ Jgrasp Files/October11.java
@@ -0,0 +1,5 @@
+public class October11{
+   public static void main(String args[]){
+      System.out.println("hi");
+   }
+}
---
+++ Jgrasp Files/October8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+public class October8 {
+   public static void main(String args[]){
+      Scanner input = new Scanner(System.in);
+      /*int x = input.nextInt();
+      int max(int x, int y){
+         if(x>y){
+            return x;
+         }else{
+            return y;
+         }
+      }
+      max(4,5); */
+   }
+}
