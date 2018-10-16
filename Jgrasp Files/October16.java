0bfbab208a79e8a5d55cb5552cc744949356f024
Nick Cleary <nicholascleary01@gmail.com>
2018-10-16 10:34:53 -0400

    Just fiddling

--- Jgrasp Files/October16.java
+++ Jgrasp Files/October16.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class October16{
+   /*public static int foo(int a, int b){
+      return a+b;
+   }
+   public static int foo(double a, int b){
+      return a*b;
+   }
    public static void main(String args[]){
-      System.out.println("hi");
+      foo(3,4);
+   }*/
+   public static void main(String args[]){
+      Scanner input = new Scanner(System.in);
+      int[] a = new int[10];
+      int i = 0;
+      int x;
+      do{
+         x = input.nextInt();
+      } while (x != 0);
+      System.out.println(a);
    }
 }
