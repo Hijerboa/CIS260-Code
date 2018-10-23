47c172ada7d50066d84d985884c60647927d53df
Nick Cleary <nicholascleary01@gmail.com>
2018-10-22 21:39:27 -0400

    Workin on that project tho

---
+++ Jgrasp Files/Driver.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+
+public class Driver
+{
+    
+    public static void main(String[] args){
+        
+        //construct DrawingPanel, and get its Graphics context
+        DrawingPanel panel = new DrawingPanel(480, 480);
+        Graphics g = panel.getGraphics();
+        
+        //Test Step 1 - construct mountain map data
+        MapDataDrawer_Cleary map = new MapDataDrawer_Cleary("Colorado_480x480.dat", 480, 480);
+        
+         //Test Step 2 - min, max, minRow in col
+         int min = map.findMinValue();
+         System.out.println("Min value in map: "+min);
+         
+         int max = map.findMaxValue();
+         System.out.println("Max value in map: "+max);
+         
+         int minRow = map.indexOfMinInCol(0);
+         System.out.println("Row with lowest val in col 0: "+minRow);
+         
+         //Test Step 3 - draw the map
+         map.drawMap(g);
+                  
+        
+    }
+
+
+}
---
+++ Jgrasp Files/MapDataDrawer_Cleary.java
@@ -0,0 +1,89 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
+public class MapDataDrawer_Cleary
+{
+
+  private int[][] grid;
+
+  public void MapDataDrawer(String filename, int rows, int cols){
+      // initialize grid 
+      grid = new int[rows][cols];
+      
+      //read the data from the file into the grid
+      File dataFile = new File(filename);
+      try {
+         Scanner dataInput = new Scanner(dataFile);
+         for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols;j++) {
+               grid[i][j] = dataInput.nextInt();
+            
+            }
+         }
+         
+      } catch (Exception e) { e.printStackTrace();}
+
+  }
+  
+  /**
+   * @return the min value in the entire grid
+   */
+  public int findMinValue() {
+     int min = Integer.MAX_VALUE;
+      // IMPLEMENT THE REST OF THIS METHOD
+      for(int i=0;i<480;i++){
+         for(int j=0;j<480;j++){
+            if(grid[i][j] < min){
+               min = grid[i][j];
+            }
+         }
+      }
+     return min;  
+  }
+  /**
+   * @return the max value in the entire grid
+   */
+  public int findMaxValue(){
+     int max = Integer.MIN_VALUE;
+     
+     // IMPLEMENT THE REST OF THIS METHOD
+      for(int i=0;i<480;i++){
+         for(int j=0;j<480;j++){
+            if(grid[i][j] > max){
+               max = grid[i][j];
+            }
+         }
+      }
+     return max;      
+  }
+  
+
+  /**
+   * Draws the grid using the given Graphics object.
+   * Colors should be grayscale values 0-255, scaled based on min/max values in grid
+   */
+  public void drawMap(Graphics g){
+      int min = findMinValue();
+      int max = findMaxValue();
+      
+      for (int i=0; i<480; i++) {
+         for (int j=0; j<480; j++) {
+            int c = 1;// calculated grayscale value FILL THIS IN
+            g.setColor(new Color(c, c, c));
+            g.fillRect(j, i, 1, 1);
+         }
+      }
+   }
+
+
+   private int minOfThree(int a, int b, int c) {
+      if ((a > b) && (a > c)) return a;
+      if ((b > a) && (b > c)) return b;
+      if ((c > a) && (c > b)) return c;
+      return 0;
+   }
+  
+  
+  
+}
