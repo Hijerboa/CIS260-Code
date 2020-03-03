9cb2788af866af9887acec0d76dad79da1ee6a28
Hijerboa <nicholascleary01@gmail.com>
2020-03-02 22:12:39 -0500

    I wonder if this will work

--- Jgrasp Files/September24.java
+++ Jgrasp Files/September24.java
@@ -1,7 +1,7 @@
 class September24{
    public static void main(String args[]){
       double aCount = 0;
-      double bCount = 0; 
+      double bCount = 0; //hello there
          for(double i = 0; i < 200; i++){
             boolean x9 = Math.random() > 0.5;
          if(x9) {
